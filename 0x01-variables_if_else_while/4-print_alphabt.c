742b9aa4bca8734c3b01ec2fdae0b351fc122909
NDTGB <sodium2007p@gmail.com>
2022-10-16 15:30:59 -0700

    added 4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - print lowercase alpha a-z remove 'q' and 'e'
+ *Return: Always 0 (success)
+ */
+int main(void)
+
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		if ((alpha != 'q') && (alpha != 'e'))
+		{
+			putchar(alpha);
+		}
+		alpha++;
+	}
+	putchar('\n');
+	return (0);
+}
