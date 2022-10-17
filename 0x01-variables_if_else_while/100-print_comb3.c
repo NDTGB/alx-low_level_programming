d720d9b1b1adb2befd984bf2c16aeff90d06c277
NDTGB <sodium2007p@gmail.com>
2022-10-16 18:02:38 -0700

    added 100-print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ *main - print 00 to 99 with no dulicated combos
+ *Return: always 0 (Sucess)
+ */
+
+int main(void)
+{
+	int ones;
+	int tens;
+
+	for (tens = '0'; tens <= '9'; tens++) /*increment tens*/
+	{
+		for (ones = (tens + 1); ones <= '9'; ones++) /*one's ten + 1*/
+		{
+			putchar(tens);
+			putchar(ones);
+
+			if (tens != '8' || ones != '9') /*print commas*/
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
