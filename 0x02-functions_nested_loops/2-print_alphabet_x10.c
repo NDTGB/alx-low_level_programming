aecc3e3891c4343b1344aa49f07983fb7db7c336
NDTGB <sodium2007p@gmail.com>
2022-10-18 08:21:47 -0700

    added

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *print_alphabet -prints alphabets 10 times
+ *print_alphabet_x10 - prints alphabets 10 times
  */
 
 void print_alphabet_x10(void)
@@ -10,7 +10,7 @@ void print_alphabet_x10(void)
 	char letter;
 	int x;
 
-	for (x = 0; x <= 10; x++)
+	for (x = 0; x >= 10; x++)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 
