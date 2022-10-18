1019e095946bb2668dd8adf8ed4df141fb6d623b
NDTGB <sodium2007p@gmail.com>
2022-10-18 13:51:20 -0700

    added

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,20 +9,19 @@ void jack_bauer(void)
 	int H = 0;
 	int m = 0;
 
+
+	while (H < 24)
+
 	{
-		for (H < "24")
-
+		while (m < 60)
 		{
-			for (m < "60")
-				_putchar('0' + (H / 10));
-				_putchar('0' + (H % 10));
-				_putchar(':');
-				_putchar('0' + (m / 10));
-				_putchar('0' + (m % 10));
-
-				_putchar ('\n');
-				m++;
-
+			_putchar('0' + (H / 10));
+			_putchar('0' + (H % 10));
+			_putchar(':');
+			_putchar('0' + (m / 10));
+			_putchar('0' + (m % 10));
+			_putchar ('\n');
+			m++;
 		}
 		m = 0;
 		H++;
