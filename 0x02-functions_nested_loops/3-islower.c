53801544b8b0010da2660d895a852d1307d286ae
NDTGB <sodium2007p@gmail.com>
2022-10-18 08:59:55 -0700

    added

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,20 +2,18 @@
 
 /**
  *_islower - a program that checks for lower case character
+ *Return: 1 if the character is lowercase but 0 if not
  */
 
 int _islower(int c)
 
 {
 	if (c >= 'a' && c <= 'z')
-	{
+
 		return (1);
-	{
+	
 	else
-	}
 
 		return (0);
-	}
 
-	_putchar('\n');
 }
