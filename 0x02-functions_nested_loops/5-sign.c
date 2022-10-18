c6de5eb6a25822278e76875fd2c63c92fbfe788c
NDTGB <sodium2007p@gmail.com>
2022-10-18 09:57:26 -0700

    added

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ *print_sign - a program that prints the sign of numbers
+ *@n: number with the sign to be printed
+ *Return: 1 if the sign is greater than zero,
+ *	0 if the sign is zero,
+ *	-1 if the sign is less than zero.
+ */
+
+int print_sign(int n)
+{
+	if (n > o)
+	{
+		_putchar('+');
+
+		return (1);
+	}
+
+	else if (n == 0)
+	{
+		_putchar('0');
+
+		return (0);
+	}
+
+	else
+	{
+		_putchar('-');
+
+		return (-1);
+
+	}
+}
