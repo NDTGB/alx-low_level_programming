3c7a49ef06206b234c236c1b68db63a744d468b9
NDTGB <sodium2007p@gmail.com>
2022-10-18 09:20:28 -0700

    added

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_isalpha - a program that checks character of alphabet
+ *@c: chracter to be examined
+ *Return: 1 if chracter lowercase and 0 if upppercase
+ */
+
+int _isalpha(int c)
+
+{
+	if ((c <= 'z' && c >= 'a ) || (c <= 'Z' &&& c >= 'A'))
+		return (1);
+	else
+		return (0);
+}
