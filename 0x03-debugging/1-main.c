967ffd4b111891a2de7e007a61520c457ddbf8ed
NDTGB <sodium2007p@gmail.com>
2022-10-20 04:37:25 -0700

    added

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,11 +12,13 @@ int main(void)
 
 	printf("Infinite loop incoming :(\n"); /*expected to loop*/
 
-	i = 0;
-	while (i >= 10) /*incoming loop expected*/
-	{
-		putchar(i);
-	}
+	/*
+	 * i = 0;
+	 * while (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
 
 	printf("Infinite loop avoided! \\o/\n");
 	return (0);
