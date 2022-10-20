0c85245ba213662cbc8dc888481358223337763e
NDTGB <sodium2007p@gmail.com>
2022-10-20 02:18:32 -0700

    added

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ *main - print 50 fibonacci numbers
+ *Return: always 0
+ */
+
+int main(void)
+{
+	int count;
+	int counter = 50;
+	long x = 1;
+	long y = 2;
+
+	for (count = 1; count <= (counter / 2); count++)
+	{
+		printf("%li %li ", a, b);
+		x += y;
+		y += x;
+	}
+	if (counter % 2 == 1)
+		printf("%li", x);
+
+	printf("\n");
+
+	return (0);
+}
