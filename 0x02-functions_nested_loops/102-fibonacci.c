5cfd0da168a52a60e070081cef025bf36c52623e
NDTGB <71469625+NDTGB@users.noreply.github.com>
2022-10-20 10:42:11 +0100

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -14,13 +14,13 @@ int main(void)
 
 	for (count = 1; count <= (counter / 2); count++)
 	{
-		printf("%li %li ", x, y);
+		printf("%li, %li ", x, y);
 		x += y;
 		y += x;
 	}
 	if (counter % 2 == 1)
 	{
-		printf("%li,", x);
+		printf("%li", x);
 	}
 	printf("\n");
 
