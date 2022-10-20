bf32e5bda5afd55d051ccc45b962928984243cff
NDTGB <sodium2007p@gmail.com>
2022-10-19 19:12:03 -0700

    added

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+
+/**
+ *print_times_table - Calculate integer from 0
+ *@n: an integer
+ */
+
+void print_times_table(int n)
+{
+	int Num, Mul, Pro;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (Num = 0; Num <= n; Num++)
+		{
+			_putchar('0');
+
+			for (Mul = 1; Mul <= n; Mul++)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				Pro = Num * Mul;
+
+				if (Pro <= 99)
+					_putchar(' ');
+				if (Pro <= 9)
+					_putchar(' ');
+
+				if (Pro >= 100)
+				{
+					_putchar((Pro / 100) + '0');
+					_putchar(((Pro / 100) % 10) + '0');
+				}
+
+				else if (Pro <= 99 && Pro >= 10)
+				{
+					_putchar((P / 100) + '0');
+				}
+
+				_putchar((Pro / 100) + '0');
+
+			}
+			_putchar('\n');
+		}
+	}
+}
