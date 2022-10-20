444bba0fd559d67a1351ac957062f1597b5c48c0
NDTGB <sodium2007p@gmail.com>
2022-10-19 20:24:30 -0700

    added

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -33,9 +33,9 @@ void print_times_table(int n)
 
 				if (Pro >= 100)
 				{
-					_putchar('0' + (Pro / 100));
-					_putchar('0' + ((Pro / 100) % 10));
-					_putchar('0' + (Pro % 100));
+					_putchar((Pro / 100) + '0');
+					_putchar(((Pro / 100) % 10) + '0');
+					_putchar((Pro % 100) + '0');
 				}
 
 				else if (Pro <= 99 && Pro >= 10)
