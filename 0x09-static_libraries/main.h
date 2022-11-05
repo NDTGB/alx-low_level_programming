cc013084a2eb5731ecf9ac50aeba34c4b1994e0a
NDTGB <sodium2007p@gmail.com>
2022-11-05 04:19:27 -0700

    added

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -21,7 +21,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /* MAIN_H */
-
-#include <stdio.h>
-#include <stdlib.h>
+#endif
